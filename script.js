6a563bfba3338c508975796cf70c8f7c6819dd3a
rishikeshreddy3515 <rishike3534d@gmail.com>
2026-01-13 16:11:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 
 
-  const username = document.getElementById("#username").value;
-  const password = document.getElementById("#password").value;
+ document.getElementById("loginbtn").onclick = function () {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
 
   if (username === "" || password === "") {
-    document.getElementById("message").innerText = "Please enter both fields";
+    document.getElementById("message").innerText =
+      "Please enter both fields";
     return;
   }
 
   if (username === "admin" && password === "1234") {
-    document.getElementById("message").innerText = "Login successful!";
+    document.getElementById("message").innerText =
+      "Login successful!";
     window.location.href = "profile.html";
   } else {
-    document.getElementById("message").innerText = "Incorrect username or password";
+    document.getElementById("message").innerText =
+      "Incorrect username or password";
   }
-
-document.getElementById("loginbtn").onclick = function () {
-  window.location.href = "profile.html";
 };
-
