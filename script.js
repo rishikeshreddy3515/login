e95f2bbe632ce77d6ebff27da8b08449e1065f65
rishikeshreddy3515 <rishike3534d@gmail.com>
2026-01-21 22:23:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,20 +4,11 @@
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
 
-  if (username === "" || password === "") {
-    document.getElementById("message").innerText =
-      "Please enter both fields";
-    return;
-  }
-
   if (username === "admin@gmail.com" && password === "1234") {
-    document.getElementById("message").innerText =
-      "Login successful!";
     window.location.href = "profile.html";
-  } else {
-    document.getElementById("message").innerText =
-      "Incorrect username or password";
-  }
+  } 
+  
 };
+
 
 
